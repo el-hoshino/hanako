040e0aebedeee1b3177d11691eae744cfc6359da
史 翔新 <x_shi@yumemi.co.jp>
2019-11-29 21:10:43 +0900

    Fix Package.swift file

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "hanako",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
+        .executable(
             name: "hanako",
             targets: ["hanako"]),
     ],
