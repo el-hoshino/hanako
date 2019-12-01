acc8c63a3a0d282e87d02cd84e517b890e9f366b
星野恵瑠 <shonetesmith@gmail.com>
2019-12-02 02:49:17 +0900

     Update printHelp method with Swift multi-line string literal format

--- Sources/hanako/Help.swift
+++ Sources/hanako/Help.swift
@@ -9,20 +9,21 @@ import Foundation
 
 func printHelp() {
     
-    let help =
-    "hanako is a random string generator.\n" +
-    "\n" +
-    "Usage:\n" +
-    "\t$ hanako [options]\n" +
-    "\n" +
-    "Arguments:\n" +
-    "\t-h / --help: Help. (This will ignore all other arguments and won't perform an output.)\n" +
-    "\t-nu / --no-uppercase: Don't use uppercased string for the output.\n" +
-    "\t-nl / --no-lowercase: Don't use lowercased string for the output.\n" +
-    "\t-nn / --no-numeral: Don't use numeral string for the output.\n" +
-    "\t-nc / --no-copy: Don't copy the result to pasteboard" +
-    "\t-l [length] / --length [length]: Output length. Replace [length] with a number. Default length is 10.\n" +
-    "\t-hf [frequency] / --hyphen-frequency [frequency]: Insert a hyphen after every certain characters. Replace [frequncy] with a number. e.g.: abc-def-ghi if you set frequency to 3, abcdefghi if you set frequncy to 0. Default frequency is 0, and hyphens are also counted in the length.\n"
+    let help = """
+    hanako is a random string generator.
+    
+    Usage:
+        $ hanako [options]
+        
+    Arguments:
+        -h / --help: Help. (This will ignore all other arguments and won't perform an output.)
+        -nu / --no-uppercase: Don't use uppercased string for the output.
+        -nl / --no-lowercase: Don't use lowercased string for the output.
+        -nn / --no-numeral: Don't use numeral string for the output.
+        -nc / --no-copy: Don't copy the result to pasteboard
+        -l [length] / --length [length]: Output length. Replace [length] with a number. Default length is 10.
+        -hf [frequency] / --hyphen-frequency [frequency]: Insert a hyphen after every certain characters. Replace [frequncy] with a number. e.g.: abc-def-ghi if you set frequency to 3, abcdefghi if you set frequncy to 0. Default frequency is 0, and hyphens are also counted in the length
+    """
     
     print(help)
     
