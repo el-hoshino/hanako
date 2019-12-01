653afc3ce339b6e3d95d23e99b5545ee3cee3a13
星野恵瑠 <shonetesmith@gmail.com>
2019-12-02 02:51:27 +0900

    Extract parsing method to a Parser class

--- Sources/hanako/main.swift
+++ Sources/hanako/main.swift
@@ -8,85 +8,31 @@
 
 import Foundation
 
-private var types: Set<Character.Kind> = [.uppercasedAlphabet, .lowercasedAlphabet, .numeric]
-private var shouldCopyToPasteboard = true
-private var length = 10
-private var hyphenFrequency = 0
+let arguments = CommandLine.arguments
+let parser = Parser()
 
-func parseCommand() {
-	
-	let arguments = Array(CommandLine.arguments.dropFirst())
-	
-	if arguments.contains("-h") || arguments.contains("--help") {
-		
-		printHelp()
-		
-		exit(EXIT_SUCCESS)
-		
-	}
-	else if arguments.contains("-v") || arguments.contains("--version") {
-		
-		printVersion()
-		
-		exit(EXIT_SUCCESS)
-		
-	}
-	else {
-		var arguments = arguments
-		while let argument = arguments.first {
-			switch argument {
-			case "-nu", "--no-uppercase":
-				types.remove(.uppercasedAlphabet)
-				arguments.removeFirst()
-				
-			case "-nl", "--no-lowercase":
-				types.remove(.lowercasedAlphabet)
-				arguments.removeFirst()
-				
-			case "-nn", "--no-numeral":
-				types.remove(.numeric)
-				arguments.removeFirst()
-				
-			case "-nc", "--no-copy":
-				shouldCopyToPasteboard = false
-				arguments.removeFirst()
-				
-			case "-l", "--length":
-				guard arguments.count > 1, let lengthParameter = Int(arguments[1]) else {
-					printError()
-					exit(EXIT_FAILURE)
-				}
-				length = lengthParameter
-				arguments.removeFirst(2)
-				
-			case "-hf", "--hyphen-frequency":
-				guard arguments.count > 1, let frequencyParameter = Int(arguments[1]) else {
-					printError()
-					exit(EXIT_FAILURE)
-				}
-				hyphenFrequency = frequencyParameter
-				arguments.removeFirst(2)
-				
-			default:
-				printError()
-				exit(EXIT_FAILURE)
-			}
-		}
-		
-        let result = String.randomString(ofLength: length, from: types, hyphenFrequency: hyphenFrequency)
-		print("Generated string: \(result)")
-		
-		if shouldCopyToPasteboard {
-			copyStringToPasteboard(result)
-			print("Result has been copied to your clipboard, you can use cmd + v to paste it.")
-		}
-		
-		print()
-		
-		exit(EXIT_SUCCESS)
-		
-	}
-	
+do {
+    let result = try parser.parse(arguments)
+    switch result {
+    case .showHelp:
+        printHelp()
+        
+    case .showVersion:
+        printVersion()
+        
+    case .generateString(let result):
+        let resultString = result.generatedString
+        print("Generated string: \(resultString)")
+        if result.settings.shouldCopyToPasteboard {
+            copyStringToPasteboard(resultString)
+            print("Result has been copied to your clipboard, you can use cmd + v to paste it.")
+        }
+    }
+    
+    print()
+    exit(EXIT_SUCCESS)
+    
+} catch let error {
+    printError(error)
+    exit(EXIT_FAILURE)
 }
-
-parseCommand()
