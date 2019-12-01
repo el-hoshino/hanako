7f5849c23f88b3666a92813cbf1a44f5c13d01e1
星野恵瑠 <shonetesmith@gmail.com>
2019-12-02 01:44:34 +0900

    Extract String and Character methods to separated String+ and Character+ extension files

---
+++ Sources/hanako/Character+.swift
@@ -0,0 +1,33 @@
+//
+//  Character+.swift
+//  hanako
+//
+//  Created by 史翔新 on 2019/12/02.
+//
+
+import Foundation
+
+extension Character {
+    
+    static let uppercaseCharacters: Set<Character> = {
+        let string = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        return string.characterSet
+    }()
+
+    static let lowercasedCharacters: Set<Character> = {
+        let string = "abcdefghijklmnopqrstuvwxyz"
+        return string.characterSet
+    }()
+
+    static let numericCharacters: Set<Character> = {
+        let string = "0123456789"
+        return string.characterSet
+    }()
+
+    enum Kind {
+        case uppercasedAlphabet
+        case lowercasedAlphabet
+        case numeric
+    }
+    
+}
--- Sources/hanako/main.swift
+++ Sources/hanako/main.swift
@@ -8,28 +8,6 @@
 
 import Foundation
 
-private extension String {
-	
-	var characterSet: Set<Character> {
-		return reduce(into: Set<Character>()) { (result, character) in
-			result.insert(character)
-		}
-	}
-	
-	func containsAnyCharacterInSet(_ array: Set<Character>) -> Bool {
-		
-		for character in array {
-			if self.contains(character) {
-				return true
-			}
-		}
-		
-		return false
-		
-	}
-	
-}
-
 extension Set where Element == Character {
 	
 	func unsafeRandomElement() -> Element {
@@ -41,98 +19,10 @@ extension Set where Element == Character {
 	
 }
 
-private let uppercaseStrings: Set<Character> = {
-	let string = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-	return string.characterSet
-}()
-
-private let lowercasedStrings: Set<Character> = {
-	let string = "abcdefghijklmnopqrstuvwxyz"
-	return string.characterSet
-}()
-
-private let numberStrings: Set<Character> = {
-	let string = "0123456789"
-	return string.characterSet
-}()
-
-enum CharacterType {
-	case uppercasedAlphabet
-	case lowercasedAlphabet
-	case numeral
-}
-
-private var types: Set<CharacterType> = [.uppercasedAlphabet, .lowercasedAlphabet, .numeral]
+private var types: Set<Character.Kind> = [.uppercasedAlphabet, .lowercasedAlphabet, .numeric]
 private var shouldCopyToPasteboard = true
 private var length = 10
 private var hyphenFrequency = 0
-
-func createRandomString(ofLength length: Int, from types: Set<CharacterType>, hyphenFrequency: Int) -> String {
-	
-	guard types.count > 0 else {
-		let errorMessage = "No characters able to use in random string, please check your setting.\n"
-		print(errorMessage)
-		exit(EXIT_FAILURE)
-	}
-	
-	let characterList = types.reduce(into: Set<Character>()) { (set, type) in
-		let nextList: Set<Character>
-		switch type {
-		case .uppercasedAlphabet:
-			nextList = uppercaseStrings
-			
-		case .lowercasedAlphabet:
-			nextList = lowercasedStrings
-			
-		case .numeral:
-			nextList = numberStrings
-		}
-		set.formUnion(nextList)
-	}
-	
-	let randomString: String
-	if hyphenFrequency > 0 {
-		let hyphenPosition = hyphenFrequency + 1
-		randomString = (1 ... length).reduce("") { (string, index) -> String in
-			if index % hyphenPosition == 0 {
-				return string + "-"
-			} else {
-				return string + "\(characterList.unsafeRandomElement())"
-			}
-		}
-		
-	} else {
-		randomString = (1 ... length).reduce("") { (string, _) -> String in
-			return string + "\(characterList.unsafeRandomElement())"
-		}
-	}
-	
-	if length >= types.count {
-		
-		for type in types {
-			
-			let characterSet: Set<Character>
-			switch type {
-			case .uppercasedAlphabet:
-				characterSet = uppercaseStrings
-				
-			case .lowercasedAlphabet:
-				characterSet = lowercasedStrings
-				
-			case .numeral:
-				characterSet = numberStrings
-			}
-			
-			guard randomString.containsAnyCharacterInSet(characterSet) else {
-				return createRandomString(ofLength: length, from: types, hyphenFrequency: hyphenFrequency)
-			}
-			
-		}
-	}
-	
-	return randomString
-	
-}
 
 func parseCommand() {
 	
@@ -165,7 +55,7 @@ func parseCommand() {
 				arguments.removeFirst()
 				
 			case "-nn", "--no-numeral":
-				types.remove(.numeral)
+				types.remove(.numeric)
 				arguments.removeFirst()
 				
 			case "-nc", "--no-copy":
@@ -194,7 +84,7 @@ func parseCommand() {
 			}
 		}
 		
-		let result = createRandomString(ofLength: length, from: types, hyphenFrequency: hyphenFrequency)
+        let result = String.randomString(ofLength: length, from: types, hyphenFrequency: hyphenFrequency)
 		print("Generated string: \(result)")
 		
 		if shouldCopyToPasteboard {
