7e1becb38df02fe4aefd89663552f3de4c3a1297
史 翔新 <x_shi@yumemi.co.jp>
2019-12-04 19:16:14 +0900

    Add tests to Character

---
+++ Tests/hanakoTests/CharacterTests.swift
@@ -0,0 +1,41 @@
+//
+//  CharacterTests.swift
+//  hanakoTests
+//
+//  Created by 史 翔新 on 2019/12/04.
+//
+
+import XCTest
+@testable import hanako
+
+final class CharacterTests: XCTestCase {
+    
+    func testCharacterSet() {
+        
+        typealias TestCase = (characterKind: Character.Kind, shouldContainingCharacters: [Character], shouldNotContainingCharacters: [Character])
+        let testCases: [TestCase] = [
+            (.uppercasedAlphabet, "ABC".characters, "abc123".characters),
+            (.lowercasedAlphabet, "abc".characters, "ABC123".characters),
+            (.numeric, "123".characters, "ABCabc".characters)
+        ]
+        
+        for testCase in testCases {
+            testCase.shouldContainingCharacters.forEach {
+                XCTAssertTrue(testCase.characterKind.characterSet.contains($0))
+            }
+            testCase.shouldNotContainingCharacters.forEach {
+                XCTAssertFalse(testCase.characterKind.characterSet.contains($0))
+            }
+        }
+                
+    }
+    
+}
+
+private extension String {
+    
+    var characters: [Character] {
+        return map({ $0 })
+    }
+    
+}
